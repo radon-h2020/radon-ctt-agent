8e9609762b74d168f446fcc7cc6b7ab61d410638
Thomas F. Düllmann <duellmann@iste.uni-stuttgart.de>
2020-06-03 11:43:47 +0200

    Added lookup for plugin requirements.

--- run_ctt_agent.sh
+++ run_ctt_agent.sh
@@ -14,6 +14,13 @@ source $VENV_DIR/bin/activate
 # Install requirements
 pip install --no-cache -r requirements.txt
 
+# Look for requirements.txt files in the 'plugins' folder and install them
+PLUGIN_REQ_FILES=$(find plugins -iname "requirements.txt" -type f)
+for req_file in $PLUGIN_REQ_FILES
+do
+   pip install --no-cache -r $req_file
+done
+
 # Start CTT Server
 python ctt-agent.py
 
@@ -21,3 +28,4 @@ deactivate
 
 # Return to original folder
 cd $CURRENT_DIR
+
